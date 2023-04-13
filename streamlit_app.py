a332d3f69f0ec806ad04ce297086203d0bddbe5b
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-13 12:04:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-my_cur.execute("select FILE_URL as file_URL from directory(@SUPER_MARKET_SALES.RECORDS.internal_stage_img) where FILE_URL="" ")
+my_cur.execute("select FILE_URL as file_URL from directory(@SUPER_MARKET_SALES.RECORDS.internal_stage_img)")
 my_catalog = my_cur.fetchall()
 
 df = pandas.DataFrame(my_catalog)
