45e8c6a566097b5f38200772ae40efe7c797f874
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-13 12:03:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-my_cur.execute("select FILE_URL as file_URL from directory(@SUPER_MARKET_SALES.RECORDS.internal_stage_img)")
+my_cur.execute("select FILE_URL as file_URL from directory(@SUPER_MARKET_SALES.RECORDS.internal_stage_img) where FILE_URL="" ")
 my_catalog = my_cur.fetchall()
 
 df = pandas.DataFrame(my_catalog)
@@ -14,8 +14,7 @@ streamlit.write(df)
 
 streamlit.image(
       df[0],
-      width=400,
-      caption= product_caption
+      width=400
   )
 
 
